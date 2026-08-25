02a7cc6713109db7dea1b85c49a90cb543a776c3
Jeff Yankson <jjyankson19@gmail.com>
2026-08-25 17:55:46 +0000

    Move route logic into router & import router

--- app.js
+++ app.js
@@ -1,46 +1,15 @@
 import express from 'express';
 import { configDotenv } from 'dotenv';
 
+// import routes
+import router from './routes/bootcamp.routes.js';
+
 configDotenv({ path: './config/config.env' });
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// create routes
-app.get('/api/v1/bootcamps', (req, res) => {
-  res.json({
-    enroll: true,
-    msg: `GET bootcamp ${req.params.id}`,
-  });
-});
-
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-  res.json({
-    enroll: true,
-    msg: `GET single bootcamp ${req.params.id}`,
-  });
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.status(201).json({
-    enroll: true,
-    msg: `POST bootcamp`,
-  });
-});
-
-app.patch('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({
-    success: true,
-    msg: `Update bootcamp ${req.params.id}`,
-  });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res.json({
-    success: true,
-    msg: `Delete bootcamp ${req.params.id}`,
-  });
-});
+app.use('/api/v1/bootcamps', router)
 
 app.listen(
   PORT,
