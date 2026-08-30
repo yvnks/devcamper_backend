93a47e6b68cc1c36fcc60811b6f225353aea06c2
Jeff Yankson <jjyankson19@gmail.com>
2026-08-30 06:22:35 +0000

    Handle mongoose duplicate errors

--- middleware/customErrorHandler.js
+++ middleware/customErrorHandler.js
@@ -4,10 +4,18 @@ const customErrorHandler = (err, req, res, next) => {
   let error = { ...err };
   error.message = err.message;
 
+  console.log(error);
+
   // mongoose bad objectId
   if (err.name === "CastError") {
     const message = `Resource not found with ID of ${err.value}`;
     error = new CustomErrorHandlerAPI(message, 404);
+  }
+
+  if (err.code === 11000) {
+    const message = "Duplicate fields found.";
+    // bad req
+    error = new CustomErrorHandlerAPI(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
